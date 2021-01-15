18e80161051ca047aa349a74c8e7397edc6fb8cb
Nikolay Parshakov <prognik97@gmail.com>
2021-01-15 15:21:04 +0300

    add middleware for getting request attribute in action

---
+++ app/Middlewares/RequestAttributeMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Middlewares;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Routing\RouteContext;
+
+class RequestAttributeMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $route = RouteContext::fromRequest($request)->getRoute();
+        foreach ($route->getArguments() as $key => $val) {
+            $request = $request->withAttribute($key, $val);
+        }
+
+        return $handler->handle($request);
+    }
+}
