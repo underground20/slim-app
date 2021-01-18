95ea7c674b0af3d71e674638ecbdd35dbed67379
Nikolay Parshakov <prognik97@gmail.com>
2021-01-18 18:53:49 +0300

    create entity user

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -6,27 +6,21 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @@ORM\Table(name="tasks")
+ * @@ORM\Table(name="users")
  */
-class Task
+class User
 {
     /**
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue
-     * @ORM\Id
-     */
+    * @ORM\Column(type="integer")
+    * @ORM\GeneratedValue
+    * @ORM\Id
+    */
     private $id;
 
     /**
      * @ORM\Column(type="string")
      */
     private $name;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="User")
-     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
-     */
-    private $userId;
 
     public function getId()
     {
