3dc3866d18e45fe32aae36554aedd68c7d700edb
Nikolay Parshakov <prognik97@gmail.com>
2021-01-19 14:18:34 +0300

    add faker generator

--- app/Fixtures/TaskFixture.php
+++ app/Fixtures/TaskFixture.php
@@ -6,13 +6,16 @@ use App\Entity\Task;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class TaskFixture extends AbstractFixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create();
+
         $task = new Task();
-        $task->setName('do some');
+        $task->setName($faker->domainWord);
         $task->setUserId($this->getReference('user-id'));
         $manager->persist($task);
         $manager->flush();
--- app/Fixtures/UserFixture.php
+++ app/Fixtures/UserFixture.php
@@ -5,13 +5,16 @@ namespace App\Fixtures;
 use App\Entity\User;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class UserFixture extends AbstractFixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create();
+
         $user = new User();
-        $user->setName('John');
+        $user->setName($faker->userName);
 
         $manager->persist($user);
         $manager->flush();
